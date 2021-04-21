1339f59924766597ac5f2338b42e4c7cfe5b92f5
vasadaz <vasadaz@ya.ru>
2021-04-21 18:22:34 +0300

    Optimized http and https, ftp, term. Added local and utc time.

--- ftp_script.py
+++ ftp_script.py
@@ -1,16 +1,35 @@
 import os
+import time
 
-print("\n\n\nFTP start")
-print("----------------------------------------------------------------------------")
 
-print("\n Download ts.zip\n")
-os.system("wget ftp://alta.ru/packets/distr/ts.zip")
+def cmd_time():
+    # Месное время
+    local_time = time.localtime()
+    local_time_str = "{:0>2d}:{:0>2d}:{:0>2d}".format(local_time.tm_hour, local_time.tm_min, local_time.tm_sec)
 
-print("\n\nDownload gtdw.zip\n")
-os.system("wget ftp://alta.ru/packets/distr/gtdw.zip")
+    # UTC время
+    utc_time = time.gmtime()
+    utc_time_str = "{:0>2d}:{:0>2d}:{:0>2d}".format(utc_time.tm_hour, utc_time.tm_min, utc_time.tm_sec)
 
-print("\n\nDownload maximum.zip\n")
-os.system("wget ftp://alta.ru/packets/distr/maximum.zip")
+    return "{} (utc {})".format(local_time_str, utc_time_str)
 
-print("\n----------------------------------------------------------------------------")
-print("FTP end")
+
+def ftp_test(download_list: list):
+    print("\n\nFTP start")
+    print("----------------------------------------------------------------------------")
+
+    for el in download_list:
+        print("\n{}\nFTP {}".format(cmd_time(), el))
+        print("Download {}\n".format(el[28:]))
+        os.system("wget {}".format(el))
+        time.sleep(60)
+
+    print("\n----------------------------------------------------------------------------")
+    print("FTP end")
+    return
+
+
+ftp_list = ["ftp://alta.ru/packets/distr/ts.zip",
+            "ftp://alta.ru/packets/distr/gtdw.zip",
+            "ftp://alta.ru/packets/distr/maximum.zip"]
+ftp_test(ftp_list)
--- term_script.py
+++ term_script.py
@@ -3,51 +3,39 @@ import time
 
 
 def cmd_time():
-    a = str(subprocess.check_output("date /t", shell=True).strip()).strip("b'")
-    b = str(subprocess.check_output("time /t", shell=True).strip()).strip("b'")
-    time.sleep(1)
-    return str(a + " " + b)
+    # Месное время
+    local_time = time.localtime()
+    local_time_str = "{:0>2d}:{:0>2d}:{:0>2d}".format(local_time.tm_hour, local_time.tm_min, local_time.tm_sec)
 
-my_timeout = 10
+    # UTC время
+    utc_time = time.gmtime()
+    utc_time_str = "{:0>2d}:{:0>2d}:{:0>2d}".format(utc_time.tm_hour, utc_time.tm_min, utc_time.tm_sec)
 
-print("\n\n\nTELNET start")
-print("----------------------------------------------------------------------------")
-
-print("\n{}\nTELNET connecting towel.blinkenlights.nl ip:213.136.8.188".format(cmd_time()))
-print("NOT CONNECTION")
-subprocess.Popen(["putty", "-telnet", "towel.blinkenlights.nl"])
-time.sleep(my_timeout)
-subprocess.run("taskkill /IM putty.exe /F")
-
-cmd_time()
-print("\n{}\nTELNET connecting lord.stabs.org ip:192.241.222.161".format(cmd_time()))
-subprocess.Popen(["putty", "-telnet", "lord.stabs.org"])
-time.sleep(my_timeout)
-subprocess.run("taskkill /IM putty.exe /F")
-
-cmd_time()
-print("\n{}\n\TELNET connecting 35.185.12.150".format(cmd_time()))
-subprocess.Popen(["putty", "-telnet", "35.185.12.150"])
-subprocess.run("taskkill /IM putty.exe /F")
-time.sleep(my_timeout)
-subprocess.run("taskkill /IM putty.exe /F")
-
-print("----------------------------------------------------------------------------")
-print("TELNET end")
-
-print("\n\nSSH start")
-print("----------------------------------------------------------------------------")
-
-print("\n{}\nSSH connecting 195.144.107.198".format(cmd_time()))
-subprocess.Popen(["putty", "-ssh", "195.144.107.198"])
-time.sleep(my_timeout)
-subprocess.run("taskkill /IM putty.exe /F")
+    return "{} (utc {})".format(local_time_str, utc_time_str)
 
 
-print("\n{}\nSSH connecting sdf.org ip:205.166.94.16".format(cmd_time()))
-subprocess.Popen(["putty", "-ssh", "sdf.org"])
-time.sleep(my_timeout)
-subprocess.run("taskkill /IM putty.exe /F")
+def terminal_test(protocol: str, hosts_list: list):
+    print("\n\n{} start".format(protocol))
+    print("----------------------------------------------------------------------------")
 
-print("\n----------------------------------------------------------------------------")
-print("SSH end")
+    flag = "-telnet" if len(protocol) > 3 else "-ssh"
+
+    for el in hosts_list:
+        print("\n{}\n{} {}".format(cmd_time(), protocol, el))
+        subprocess.Popen(["putty", flag, el])
+        time.sleep(10)
+        subprocess.run("taskkill /IM putty.exe /F")
+
+    print("\n----------------------------------------------------------------------------")
+    print("{} end".format(protocol))
+    return
+
+
+telnet_list = ["towel.blinkenlights.nl",
+               "lord.stabs.org",
+               "35.185.12.150"]
+terminal_test("TELNET", telnet_list)
+
+ssh_list = ["195.144.107.198",
+            "sdf.org"]
+terminal_test("SSH", ssh_list)
