7287fc823719540ef2f6402200e65699fb55813c
vasadaz <vasadaz@ya.ru>
2021-04-21 12:50:56 +0300

    Update for term

--- http_script.py
+++ http_script.py
@@ -7,40 +7,42 @@ print("-------------------------------------------------------------------------
 # инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
 driver = webdriver.Chrome()
 
+my_timeout = 10
+
 # команда time.sleep устанавливает паузу в 5 секунд, чтобы мы успели увидеть, что происходит в браузере
-time.sleep(5)
+time.sleep(my_timeout)
 
 # Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
 driver.get("http://kremlin.ru")
-time.sleep(5)
+time.sleep(my_timeout)
 
 driver.get("http://kremlin.ru/acts/constitution")
-time.sleep(5)
+time.sleep(my_timeout)
 
 driver.get("http://kremlin.ru/static/pdf/constitution.pdf?6fbd2dc717")
-time.sleep(5)
+time.sleep(my_timeout)
 
 driver.get("http://fsb.ru")
-time.sleep(5)
+time.sleep(my_timeout)
 
 driver.get("http://khann.ru")
-time.sleep(5)
+time.sleep(my_timeout)
 
 driver.get("http://khann.ru/wallpapers/")
-time.sleep(5)
+time.sleep(my_timeout)
 
 driver.get("http://alex-kuznetsov.ru/test")
-time.sleep(5)
+time.sleep(my_timeout)
 
 try:
     driver.get("http://www.thesheep.info")
-    time.sleep(5)
+    time.sleep(my_timeout)
 except:
     pass
 
 try:
     driver.get("http://www.grani.ru")
-    time.sleep(5)
+    time.sleep(my_timeout)
 except:
     pass
 
--- term_script.py
+++ term_script.py
@@ -1,28 +1,53 @@
-import os
+import subprocess
+import time
 
-print("\n\n\nTERMINAL TELNET start")
+
+def cmd_time():
+    a = str(subprocess.check_output("date /t", shell=True).strip()).strip("b'")
+    b = str(subprocess.check_output("time /t", shell=True).strip()).strip("b'")
+    time.sleep(1)
+    return str(a + " " + b)
+
+my_timeout = 10
+
+print("\n\n\nTELNET start")
 print("----------------------------------------------------------------------------")
 
-print("\nTELNET connecting towel.blinkenlights.nl")
-os.system("putty -telnet towel.blinkenlights.nl")
+print("\n{}\nTELNET connecting towel.blinkenlights.nl ip:213.136.8.188".format(cmd_time()))
+print("NOT CONNECTION")
+subprocess.Popen(["putty", "-telnet", "towel.blinkenlights.nl"])
+time.sleep(my_timeout)
+subprocess.run("taskkill /IM putty.exe /F")
 
-print("\nTELNET connecting lord.stabs.org")
-os.system("putty -telnet  lord.stabs.org")
+cmd_time()
+print("\n{}\nTELNET connecting lord.stabs.org ip:192.241.222.161".format(cmd_time()))
+subprocess.Popen(["putty", "-telnet", "lord.stabs.org"])
+time.sleep(my_timeout)
+subprocess.run("taskkill /IM putty.exe /F")
 
-print("\n\TELNET connecting 35.185.12.150")
-os.system("putty -telnet 35.185.12.150")
-# taskkill /IM putty.exe /F
+cmd_time()
+print("\n{}\n\TELNET connecting 35.185.12.150".format(cmd_time()))
+subprocess.Popen(["putty", "-telnet", "35.185.12.150"])
+subprocess.run("taskkill /IM putty.exe /F")
+time.sleep(my_timeout)
+subprocess.run("taskkill /IM putty.exe /F")
+
 print("----------------------------------------------------------------------------")
-print("TERMINAL TELNET end")
+print("TELNET end")
 
-print("\n\n\nTERMINAL SSH start")
+print("\n\nSSH start")
 print("----------------------------------------------------------------------------")
 
-print("\nSSH connecting 195.144.107.198")
-os.system("putty -ssh  195.144.107.198")
+print("\n{}\nSSH connecting 195.144.107.198".format(cmd_time()))
+subprocess.Popen(["putty", "-ssh", "195.144.107.198"])
+time.sleep(my_timeout)
+subprocess.run("taskkill /IM putty.exe /F")
 
-print("\n\SSH connecting sdf.org")
-os.system("putty -ssh sdf.org")
+
+print("\n{}\nSSH connecting sdf.org ip:205.166.94.16".format(cmd_time()))
+subprocess.Popen(["putty", "-ssh", "sdf.org"])
+time.sleep(my_timeout)
+subprocess.run("taskkill /IM putty.exe /F")
 
 print("\n----------------------------------------------------------------------------")
-print("TERMINAL SSH end")
+print("SSH end")
