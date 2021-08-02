55065d1ec3d6bbe62d537b66fdc7cfa40007499d
Sysoev <vasadaz@ya.ru>
2021-08-02 11:56:56 +0300

    Start email test

---
+++ test_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Скрипт для автоматического тестирования email тестов 573.
+Логирование команд просисходит в консоле.
+Работа с POP3, IMAP, SMTP
+
+Источник:
+https://www.code-learner.com/python-use-pop3-to-read-email-example/
+"""
