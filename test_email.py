f06af40af86e12a7120b14c0af5f9b5c4516d1c6
Sysoev <vasadaz@ya.ru>
2021-08-02 14:15:17 +0300

    Update information's source

--- test_email.py
+++ test_email.py
@@ -2,8 +2,10 @@
 """
 Скрипт для автоматического тестирования email тестов 573.
 Логирование команд просисходит в консоле.
-Работа с POP3, IMAP, SMTP
+Работа с IMAP, SMTP, POP3
 
 Источник:
-https://www.code-learner.com/python-use-pop3-to-read-email-example/
+SMTP https://code.tutsplus.com/ru/tutorials/sending-emails-in-python-with-smtp--cms-29975
+IMAP http://python-3.ru/page/imap-email-python
+POP3 https://www.code-learner.com/python-use-pop3-to-read-email-example/
 """
