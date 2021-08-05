a61e91ed6324c58507633b54b30d02a4cf9b2d1b
Sysoev <vasadaz@ya.ru>
2021-08-05 10:51:16 +0300

    Edit information's source

--- test_email_smtp.py
+++ test_email_smtp.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
 Скрипт для автоматического тестирования email тестов 573.
-Логирование команд просисходит в консоле.
-Работа с IMAP, SMTP, POP3
+Логирование команд происходит в консоле.
+Работа с SMTP
 
 Источник:
 https://habr.com/ru/post/51772/
@@ -10,12 +10,8 @@ https://habr.com/ru/company/truevds/blog/262819/
 
 SMTP https://code.tutsplus.com/ru/tutorials/sending-emails-in-python-with-smtp--cms-29975
      https://habr.com/ru/post/495256/
-IMAP http://python-3.ru/page/imap-email-python
-POP3 https://www.code-learner.com/python-use-pop3-to-read-email-example/
+
 """
-
-# import necessary packages
-
 
 import smtplib  # Импортируем библиотеку по работе с SMTP
 from email import encoders  # Импортируем энкодер
