b627ea02f4fe6a483702cc7c50784322850d518a
Sysoev <vasadaz@ya.ru>
2021-08-05 10:49:44 +0300

    Added new file for POP3

---
+++ test_email_pop3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Скрипт для автоматического тестирования email тестов 573.
+Логирование команд происходит в консоле.
+Работа с POP3
+
+Источник:
+https://habr.com/ru/post/51772/
+https://habr.com/ru/company/truevds/blog/262819/
+
+POP3 https://www.code-learner.com/python-use-pop3-to-read-email-example/
+"""
+
+
