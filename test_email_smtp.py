0316f7329bc2a7474bf82b1d9819223956283f17
Sysoev <vasadaz@ya.ru>
2021-08-06 12:44:05 +0300

    Edit comments

--- test_email_smtp.py
+++ test_email_smtp.py
@@ -87,38 +87,39 @@ def send_email(list_from: list, list_to: list, list_msg: list, list_cc=None, lis
     time.sleep(10)
 
 
-# Отравители
+# Отравитель №1
 sender_1 = ["test@rtc-nt.ru", "Elcom101120", "mail.nic.ru", "587"]
-sender_2 = ["rtc-nt-test1@yandex.ru", "zaq123edcxsw2", "smtp.yandex.ru", "587"]
-
 # Письмо №1
 to_1 = ["rtc-nt-test1@yandex.ru", "rtc-nt-test2@yandex.ru", "rtc-nt-test3@yandex.ru"]
 bcc_1 = ["rtc-nt-test4@yandex.ru"]
-msg_1 = ["Отправка письма с 3 получателями, вложением, скрытой копией",  # Тема письма
+msg_1 = ["АВТО Отправка письма с 3 получателями, вложением и скрытой копией",  # Тема письма
          "Текст письма Python",  # Текст письма
          "constitution.pdf"]  # Прикреплённый файл из ./email/
-
-# Письмо №2
-to_2 = ["test@rtc-nt.ru", "rtc-nt-test2@yandex.ru", "rtc-nt-test3@yandex.ru"]
-msg_2 = ["АВТО Получение письма с 3 получателями,вложением\r\n",  # Тема письма
-         "Текст письма",  # Текст письма
-         "constitution.pdf"]  # Прикреплённый файл из ./email/
-
 # Письмо №3
 to_3 = ["rtc-nt-test4@yandex.ru"]
 cc_3 = ["rtc-nt-test2@yandex.ru", "rtc-nt-test3@yandex.ru"]
-msg_3 = ["ОТ питона",  # Тема письма
-         "АВТО текст механизм PLAINَ"]  # Текст письма
+msg_3 = ["АВТО Отправка письма с 2 копиями и иероглифами",  # Тема письма
+         "لِيَتَقَدَّسِ اسْمُكَ"]  # Текст письма
 
+# Отравитель №2
+sender_2 = ["rtc-nt-test1@yandex.ru", "zaq123edcxsw2", "smtp.yandex.ru", "587"]
+# Письмо №2
+to_2 = ["test@rtc-nt.ru", "rtc-nt-test2@yandex.ru", "rtc-nt-test3@yandex.ru"]
+msg_2 = ["АВТО Получение письма с 3 получателями и вложением\r\n",  # Тема письма
+         "Текст письма",  # Текст письма
+         "constitution.pdf"]  # Прикреплённый файл из ./email/
 # Письмо №4
 to_4 = ["test@rtc-nt.ru"]
 cc_4 = ["rtc-nt-test2@yandex.ru", "rtc-nt-test3@yandex.ru"]
-msg_4 = ["АВТО Получение письма с 2 копиями и иероглифы\r\n",  # Тема письма
+msg_4 = ["АВТО Получение письма с 2 копиями и иероглифами\r\n",  # Тема письма
          "لِيَتَقَدَّسِ اسْمُكَ"]  # Текст письма
 
+# Условие для инициирования переписки
 if I_FIRST:
+    # Отравитель №1
     send_email(sender_1, to_1, msg_1, list_bcc=bcc_1)  # Отправка Письма №1
     send_email(sender_1, to_3, msg_3)  # Отправка Письма №3
 else:
+    # Отравитель №12
     send_email(sender_2, to_2, msg_2)  # Отправка Письма №2
-    #send_email(sender_2, to_4, msg_4, list_cc=cc_4)  # Отправка Письма №4
+    send_email(sender_2, to_4, msg_4, list_cc=cc_4)  # Отправка Письма №4
