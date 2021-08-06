b9edc06e9cc6f7b3714ed1a170a57cb1556cc3a1
Sysoev <vasadaz@ya.ru>
2021-08-06 15:23:51 +0300

    IMAP start

---
+++ test_email_imap.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Скрипт для автоматического тестирования email тестов 573.
+Логирование команд происходит в консоле.
+Работа с IMAP
+
+Источник:
+https://habr.com/ru/post/51772/
+https://habr.com/ru/company/truevds/blog/262819/
+
+IMAP https://yandex.ru/search/?text=python+imap&lr=2&clid=2186621
+
+"""
