57361cdba91a4808990b0cc65b20893c13626eb2
Sysoev <vasadaz@ya.ru>
2021-08-09 14:05:01 +0300

    Edit comments

--- test_email_imap.py
+++ test_email_imap.py
@@ -91,6 +91,15 @@ def imap_email(host: list):
     imap_email(host)
 
 
+
+
+
+
+
+
+
+
+
 # Отравитель
 sender = ["rtc-nt-test1@yandex.ru", "zaq123edcxsw2", "imap.yandex.ru"]
 
--- test_email_smtp.py
+++ test_email_smtp.py
@@ -16,11 +16,14 @@ SMTP https://code.tutsplus.com/ru/tutorials/sending-emails-in-python-with-smtp--
 
 
 import smtplib  # Импортируем библиотеку по работе с SMTP
+from email import message_from_string
 from email import encoders  # Импортируем энкодер
 from email.mime.base import MIMEBase  # Общий тип файла
 from email.mime.text import MIMEText  # Тип для Текст/HTML
 from email.mime.multipart import MIMEMultipart  # Многокомпонентный объект
-
+import poplib
+import imaplib
+import base64
 import time
 # Функция возврата времени из файла log_time.py
 from log_time import cmd_time
@@ -28,6 +31,10 @@ from log_time import cmd_time
 
 I_FIRST = True  # True - инициатор, False - автоответчик
 #I_FIRST = False  # True - инициатор, False - автоответчик
+NEW_MILES = 0  # Маркер определения новых писем
+STOP_POP3 = 0  # Маркер завершения функции
+STOP_IMAP = 0  # Маркер завершения функции
+
 
 def send_email(list_from: list, list_to: list, list_msg: list, list_cc=None, list_bcc=None):
     # Все данные в списках должны иметь строковый тип
@@ -84,6 +91,172 @@ def send_email(list_from: list, list_to: list, list_msg: list, list_cc=None, lis
 
     server.quit()  # Выходим
     time.sleep(10)
+
+
+def pop3_email(host: list):
+    # Передаётся список el: str in [email, pass, server]
+    global I_FIRST, NEW_MILES, STOP_POP3
+
+    time.sleep(10)
+    # Условие для завершения функции
+    if STOP_POP3 == 5 and I_FIRST:
+        return
+
+    # Подключаемся к серверу, для Яндекса нужен SSL
+    server = poplib.POP3(host[2]) if I_FIRST else poplib.POP3_SSL(host[2])
+    # server.set_debuglevel(1)  # Системный лог, дебагер
+    server.user(host[0])  # Email
+    server.pass_(host[1])  # Пароль
+    # Список доступных писем и их размер, при появлении нового список растёт.
+    mails = server.list()[1]
+
+    # Условие для начального поиска новых писем, т.е. присваивается количество писем на данный момент.
+    if NEW_MILES == 0:
+        NEW_MILES = len(mails)
+
+    # Условие для определения новых писем
+    if NEW_MILES < len(mails):
+        NEW_MILES = len(mails) if I_FIRST else 0
+    else:
+        # Действие при отсутствии писем до 5 проходов
+        STOP_POP3 += 1
+        print("No new mails! ", end="*" * (6 - STOP_POP3) + "\n")
+        time.sleep(6)
+        pop3_email(host)
+        return
+
+    lines = server.retr(NEW_MILES)[1]  # Получаем тело сообщения
+    # b'\r\n'.join(lines) Подготавливаем сообщение к декодированию.
+    # decode('utf-8') Декодируем сообщение по UTF-8 -> str
+    # split("--/") создаём список на основе декодированного сообщения, элементы списка делятся по маркеру "--/"
+    msg_content = b'\r\n'.join(lines).decode('utf-8').split("--/")
+    msg_head = message_from_string(msg_content[0])  # Преобразуем str -> dict
+    # Декодируем сообщение base64 -> UTF-8 -> str
+    msg_text = base64.b64decode(msg_content[1].split()[-1]).decode('utf-8')
+    # Условие для определения вложенного файла и присвоение его имени
+    msg_file = msg_content[2].split()[8][10:-1] if len(msg_content) > 3 else None
+
+    msg_subject_decode = str()
+    for el in (msg_head.get('Subject')).split():
+        # Декодируем тему сообщения base64 -> UTF-8 -> str
+        msg_subject_decode += base64.b64decode(el[10:-2]).decode('utf-8')
+
+    print(cmd_time())
+    print(f"FROM: {msg_head.get('From')}")  # Вытаскиваем значение по ключу
+    print(f"  TO: {msg_head.get('To')}")  # Вытаскиваем значение по ключу
+    # Вытаскиваем значение по ключу если оно есть
+    print(f"  CC: {msg_head.get('Cc')}") if msg_head.get('Cc') != (None or "") else None
+    # Вытаскиваем значение по ключу если оно есть
+    print(f" BCC: {msg_head.get('Bcc')}") if msg_head.get('Bcc') != None else None
+    print(f" SUB: {msg_subject_decode}")
+    print(f"TEXT: {msg_text}")
+    print(f"FILE: {msg_file}") if msg_file != None else None  # Имя вложенного файла если оно есть
+    print()
+
+    server.quit()  # Закрываем соединение
+    pop3_email(host)
+
+
+def imap_email(host: list):
+    global NEW_MILES, STOP_IMAP
+
+    time.sleep(10)
+
+    # Условие для завершения функции
+    if STOP_IMAP == 5 and I_FIRST:
+        return
+
+    server = imaplib.IMAP4_SSL(host[2])
+    server.debug = True
+    server.login(host[0], host[1])
+
+    # Выводит список папок в почтовом ящике.
+    server.select("inbox")  # Подключаемся к папке "входящие"
+
+    dir_inbox = server.search(None, "ALL")[1]
+    id_list = dir_inbox[0].split()  # Получаем сроку номеров писем
+    latest_email_id = id_list[-1] if len(id_list) != 0 else 0  # Берем последний ID
+
+    # Условие для начального поиска новых писем, т.е. присваивается количество писем на данный момент.
+    if NEW_MILES == 0:
+        NEW_MILES = latest_email_id
+
+    # Условие для определения новых писем
+    if NEW_MILES < latest_email_id:
+        NEW_MILES = latest_email_id
+    else:
+        # Действие при отсутствии писем до 5 проходов
+        STOP_IMAP += 1
+        print("No new mails! ", end="*" * (6 - STOP_IMAP) + "\n")
+        time.sleep(6)
+        imap_email(host)
+        return
+
+    # *server.fetch(latest_email_id, "(RFC822)")[1][0]][1] Подготавливаем сообщение к декодированию путём распаковки tuple
+    # decode('utf-8') Декодируем сообщение по UTF-8 -> str
+    # split("--/") создаём список на основе декодированного сообщения, элементы списка делятся по маркеру "--/"
+    # Тело письма в необработанном виде включает в себя заголовки и альтернативные полезные нагрузки
+    msg_content = [*server.fetch(latest_email_id, "(RFC822)")[1][0]][1].decode("utf-8").split("--/")  # Получаем тело письма
+
+    msg_head = message_from_string(msg_content[0])  # Преобразуем str -> dict
+    # Декодируем сообщение base64 -> UTF-8 -> str
+    msg_text = base64.b64decode(msg_content[1].split()[-1]).decode('utf-8')
+    # Условие для определения вложенного файла и присвоение его имени
+    msg_file = msg_content[2].split()[8][10:-1] if len(msg_content) > 3 else None
+
+    msg_subject_decode = str()
+    for el in (msg_head.get('Subject')).split():
+        # Декодируем тему сообщения base64 -> UTF-8 -> str
+        msg_subject_decode += base64.b64decode(el[10:-2]).decode('utf-8')
+
+    print(cmd_time())
+    print(f"FROM: {msg_head.get('From')}")  # Вытаскиваем значение по ключу
+    print(f"  TO: {msg_head.get('To')}")  # Вытаскиваем значение по ключу
+    # Вытаскиваем значение по ключу если оно есть
+    print(f"  CC: {msg_head.get('Cc')}") if msg_head.get('Cc') != (None or "") else None
+    # Вытаскиваем значение по ключу если оно есть
+    print(f" BCC: {msg_head.get('Bcc')}") if msg_head.get('Bcc') != None else None
+    print(f" SUB: {msg_subject_decode}")
+    print(f"TEXT: {msg_text}")
+    print(f"FILE: {msg_file}") if msg_file != None else None  # Имя вложенного файла если оно есть
+    print()
+
+    server.close()  # Закрываем соединение
+    imap_email(host)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Отравитель №1
