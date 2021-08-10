249ad6622e67bfc1c73385b4c166fb99c1dd3d7e
Sysoev <vasadaz@ya.ru>
2021-08-10 14:00:54 +0300

    Update information's source

--- test_voip.py
+++ test_voip.py
@@ -6,14 +6,5 @@
 
 Источник:
 https://most-voip.readthedocs.io/en/latest/python_docs/tutorial/voip_tutorial_1.html
+https://sipsimpleclient.org/testing/
 """
-
-# add the most.voip library root dir to the current python path...
-import sys
-sys.path.append("")
-
-# import the Voip Library
-from most.voip.api import VoipLib
-
-# instanziate the lib
-my_voip = VoipLib()
