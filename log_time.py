767d2754bf3b6e90938ecfa259bd84cf18f16548
Sysoev <vasadaz@ya.ru>
2021-10-28 16:05:04 +0300

    Added a naming time for the logs.

--- log_time.py
+++ log_time.py
@@ -7,6 +7,7 @@ import time
 def cmd_time(time_or_date="time") -> str:
     # Функция для возврата местного и GMT времени.
     # time_or_date - маркер для возврата времени(time_or_date="time") или даты(time_or_date="date").
+    # Для записи наименования лога используется формат time_or_date="for_log"
     # По умолчанию возвращает время time_or_date="time".
 
     # Местное дата и время
@@ -23,15 +24,24 @@ def cmd_time(time_or_date="time") -> str:
         # GMT время
         gmt_time_str = "{:0>2d}:{:0>2d}:{:0>2d}".format(gmt_time.tm_hour, gmt_time.tm_min, gmt_time.tm_sec)
         # Возврат времени в формате "чч:мм:сс (GMT чч:мм:сс)"
-        return "{} (GMT {})".format(local_time_str, gmt_time_str)
+        return f"{local_time_str} (GMT {gmt_time_str})"
     elif time_or_date == "date":
         # Форматирование даты в привычный вид, т.е. из 6.1.21 в 06.01.21.
         # tm_mday, tm_mon, tm_year методы для возвращения единиц времени.
         # Местная дата
-        local_time_str = "{:0>2d}.{:0>2d}.{:4d}".format(local_time.tm_mday, local_time.tm_mon, local_time.tm_year)
+        local_date_str = "{:0>2d}.{:0>2d}.{:4d}".format(local_time.tm_mday, local_time.tm_mon, local_time.tm_year)
         # GMT дата
-        gmt_time_str = "{:0>2d}.{:0>2d}.{:0>2d}".format(gmt_time.tm_mday, gmt_time.tm_mon, gmt_time.tm_year)
+        gmt_date_str = "{:0>2d}.{:0>2d}.{:0>2d}".format(gmt_time.tm_mday, gmt_time.tm_mon, gmt_time.tm_year)
         # Возврат даты в формате "ДД.ММ.ГГ (GMT ДД.ММ.ГГ)"
-        return "DATE {} (GMT {})".format(local_time_str, gmt_time_str)
+        return f"DATE {local_date_str} (GMT {gmt_date_str})"
+    elif time_or_date == "for_log":
+        # Форматирование даты в привычный вид, т.е. из 6.1.21 в 06.01.21.
+        # GMT время
+        gmt_time_log = "{:0>2d}{:0>2d}{:0>2d}".format(gmt_time.tm_hour, gmt_time.tm_min, gmt_time.tm_sec)
+        # GMT дата
+        gmt_date_log = "{:0>2d}{:0>2d}{:0>2d}".format(gmt_time.tm_year, gmt_time.tm_mon, gmt_time.tm_mday)
+        # Возврат даты в формате "ГГММДДччммсс_GMT)"
+        return gmt_time_log + gmt_date_log + "_GMT"
+
     else:
         print('НЕ ВЕРНЫЙ ФОРМАТ ДЫТЫ: time_or_date="time"/"date"')
