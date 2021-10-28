f81f11fc12856d3020555423b7267c4646c456a7
Sysoev <vasadaz@ya.ru>
2021-10-28 16:14:31 +0300

    Added a naming time for the logs.

--- log_time.py
+++ log_time.py
@@ -42,6 +42,5 @@ def cmd_time(time_or_date="time") -> str:
         gmt_date_log = "{}{:0>2d}{:0>2d}".format(gmt_time.tm_year - 2000, gmt_time.tm_mon, gmt_time.tm_mday)
         # Возврат даты в формате "ГГММДД_ччммсс_GMT)"
         return  gmt_date_log + "_" + gmt_time_log + "_GMT"
-
     else:
         print('НЕ ВЕРНЫЙ ФОРМАТ ДЫТЫ: time_or_date="time"/"date"')
