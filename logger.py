48b1dfbc1aec2cc0c964e75b36315a65af89cbb6
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-09-07 14:49:20 +0300

    Release v1.6.10

--- logger.py
+++ logger.py
@@ -23,8 +23,8 @@ except ModuleNotFoundError:
     import docx
     from docx.shared import Pt  # Для работы с .docx
 
-# Release v1.6.9
-RELEASE = "v1.6.9"
+# Release v1.6.10
+RELEASE = "v1.6.10"
 
 OBJECT_NAME = "UNKNOWN"
 
